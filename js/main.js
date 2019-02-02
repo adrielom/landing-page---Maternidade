2d4c342abd57e0a8c5084783e1d0698f5dbe296e
Adriel <adriel.oliveira@dellead.com>
2019-02-02 16:39:55 -0300

    a bunch of changes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
 $(document).ready(function() {
-  $(".social-icon").css("font-size", "3rem");
-  console.log("document ready");
-  if (document.body.clientWidth >= 1000)
-    $("#child-reference").html("Referência<br>pediátrica");
-
-  if (document.body.clientWidth < 400) {
-    $(".section3 h1").css("font-size", "2rem");
+  if (window.innerWidth <= 576) {
+    $("#logos-div").css({ padding: 0 });
   }
 });
