031fb005354aef60d8b989951f0be19211baf458
Adriel <adriel.oliveira@dellead.com>
2019-02-05 17:38:46 -0300

    sections 2, 3, button and 4 done form mobile, tablet and desktop

--- js/main.js
+++ js/main.js
@@ -2,4 +2,48 @@ $(document).ready(function() {
   if (window.innerWidth <= 576) {
     $("#logos-div").css({ padding: 0 });
   }
+
+  console.log(window.innerWidth);
+
+  whatsappButton();
+
+  $("#whatsapp-link a").click(function() {
+    if (window.innerWidth >= 1000) {
+      alert("Este link só é funcional em aparelhos celulares");
+    }
+  });
 });
+
+function whatsappButton() {
+  let linkHref =
+    "https://api.whatsapp.com/send?phone=5585989020173&text=Fa%C3%A7a%20seu%20plano%20hapvida";
+  let linkImg = "./img/whatsapp-icon.svg";
+  let altText = "whatsapp link";
+  let classes = "social-icons img-fluid ";
+
+  if (window.innerWidth < 1000) {
+    $("#whatsapp-link").html(
+      "<a href=" +
+        linkHref +
+        "><img src=" +
+        linkImg +
+        " alt=" +
+        altText +
+        " class=" +
+        classes +
+        " /> </a>"
+    );
+  } else {
+    $("#whatsapp-link")
+      .html(
+        "<a><img src=" +
+          linkImg +
+          " alt=" +
+          altText +
+          " class=" +
+          classes +
+          " /> </a>"
+      )
+      .css("cursor", "pointer");
+  }
+}
